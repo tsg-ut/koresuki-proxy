84d3d637555f5b269c52b204e84ddcbd965b8df1
Koki Takahashi <hakatasiloving@gmail.com>
2018-11-27 00:52:58 +0900

    Implement

---
+++ index.js
@@ -0,0 +1,20 @@
+const fastify = require('fastify')();
+const axios = require('axios');
+
+require('dotenv').load();
+
+fastify.post('/', async (request, reply) => {
+  if (request.body.url !== undefined) {
+    axios.post(process.env.SLACK_WEBHOOK, {
+      channel: process.env.KORESUKI_CHANNEL,
+      text: `これすき ${request.body.url}`,
+      username: 'koresuki-bot',
+      icon_emoji: process.env.KORESUKI_EMOJI,
+      unfurl_links: true,
+    });
+    return {ok: true};
+  }
+  return {ok: false};
+});
+
+fastify.listen(process.env.PORT || 3000);
