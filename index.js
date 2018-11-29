6b2f3dfe7edb8d0cdff5ffe8ab5fed1c3ebff2e4
Koki Takahashi <hakatasiloving@gmail.com>
2018-11-30 04:49:50 +0900

    Implement threading of messages

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ require('dotenv').load();
 const crypto = require('crypto');
 const fastify = require('fastify')();
 const axios = require('axios');
+const Redis = require('redis');
+const {promisify} = require('util');
 
 const keys = require('./keys.js');
+
+const redis = Redis.createClient(process.env.REDIS_URL || 'redis://localhost:6379');
 
 fastify.post('/', async (request, reply) => {
   const user = Object.entries(keys).find(([, hash]) => (
@@ -16,13 +20,26 @@ fastify.post('/', async (request, reply) => {
   }
 
   if (typeof request.body.url === 'string') {
-    await axios.post(process.env.SLACK_WEBHOOK, {
+    const ts = await promisify(redis.get).bind(redis)(request.body.url);
+
+    const {data} = await axios.post('https://slack.com/api/chat.postMessage', {
       channel: process.env.KORESUKI_CHANNEL,
-      text: `これすき ${request.body.url}`,
+      text: `これすき ${ts === null ? request.body.url : ''}`,
       username: `${user[0]} (koresuki-bot)`,
       icon_emoji: `:${user[0]}:`,
       unfurl_links: true,
+      as_user: false,
+      ...(ts === null ? {} : {thread_ts: ts, reply_broadcast: true}),
+    }, {
+      headers: {
+        Authorization: `Bearer ${process.env.SLACK_TOKEN}`,
+      },
     });
+
+    if (ts === null) {
+      await promisify(redis.set).bind(redis)(request.body.url, data.message.ts);
+    }
+
     return {ok: true};
   }
 
