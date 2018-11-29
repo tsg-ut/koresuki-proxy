4a3a4cb2d0b6f76356dc393d288f695fddbc376b
Koki Takahashi <hakatasiloving@gmail.com>
2018-11-30 04:56:54 +0900

    comma

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
 module.exports = {
 	hakatashi: 'a734fb4166f46adb65677f90a94ac35d1c095389b2059681c842ef9eefe184ae',
-	taiyoslime: 'f7de75da5e12820c5b3daa3fcd261d21acfa940225be9ecc97a8cc02e5e184c7'
+	taiyoslime: 'f7de75da5e12820c5b3daa3fcd261d21acfa940225be9ecc97a8cc02e5e184c7',
 };
